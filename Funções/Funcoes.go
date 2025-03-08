8f85eecf83bbe7f83a5347ccec62c25f200077cb
unknown <Guilherme Lisboa>
2025-03-08 20:14:17 -0300

    função

--- Funções/Funcoes.go
+++ Funções/Funcoes.go
@@ -6,6 +6,13 @@ func somar(valor int8, valor2 int8) int8 {
 	return valor + valor2
 }
 
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+
+	return soma, subtracao
+}
+
 func main() {
 	soma := somar(10, 20)
 	fmt.Println(soma)
@@ -15,4 +22,9 @@ func main() {
 	}
 	resultado := f("teste da função")
 	fmt.Println(resultado)
+
+	resultadosSoma, resultadosSubtracao := calculosMatematicos(10, 15)
+	// _, resultadosSubtracao := calculosMatematicos(10, 15)
+
+	fmt.Println(resultadosSoma, resultadosSubtracao)
 }
